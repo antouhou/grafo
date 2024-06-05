13719fb0f322c7d1e24e5a5882b974016d452e6a
Anton Suprunchuk <anton.suprunchuk@gmail.com>
2024-06-05 13:44:23 +0700

    remove default template for the crate

--- src/lib.rs
+++ src/lib.rs
@@ -9,18 +9,3 @@ mod stroke;
 
 pub use wgpu_state::{Renderer, Color, Shape, TextLayout, TextAlignment};
 pub use stroke::Stroke;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
